1cde85d9c7af3093fd9b0c07e2857167d24e1f15
satheeshna <satheeshna.sabbidi@arcadis.com>
2025-11-27 10:55:55 +0530

    kendo project

---
+++ week6/src/App.js
@@ -0,0 +1,12 @@
+import "./components/kendo-theme.scss";
+import EventForm from "./components/EventForm";
+
+function App() {
+  return (
+    <div>
+      <EventForm />
+    </div>
+  );
+}
+
+export default App;
